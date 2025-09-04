6afc2a9b9d9d07638b63379475046257f3b55616
Lucas Santos da Costa <lsantosc@live.com>
2025-09-04 00:29:08 -0300

    Add postgresql find size of all databases

---
+++ postgresql/find_size_of_databases.sql
@@ -0,0 +1,9 @@
+select t1.datname AS db_name,
+pg_size_pretty(pg_database_size(t1.datname)) as db_size
+from pg_database t1
+order by pg_database_size(t1.datname) desc;
+
+-- Returns:
+-- db_name        | db_size
+--------------------+---------
+-- foob_development | 18 GB
